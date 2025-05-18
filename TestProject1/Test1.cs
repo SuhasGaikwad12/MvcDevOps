75466dda4cf91af00ab5aa1a7493b57559730dc6
Suhas Gaikwad <Suhas.Gaikwad@tietoevry.com>
2025-05-18 15:33:52 +0530

    Unit Test

---
+++ TestProject1/Test1.cs
@@ -0,0 +1,38 @@
+﻿namespace TestProject1
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var result = 2 + 3;
+            Assert.AreEqual(5, result);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var result = 2 + 3;
+            Assert.AreEqual(5, result);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            var result = 2 + 3;
+            Assert.AreEqual(5, result);
+
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            var result = 2 + 3;
+            Assert.AreEqual(5, result);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            var result = 2 + 3;
+            Assert.AreEqual(5, result);
+        }
+    }
+}
